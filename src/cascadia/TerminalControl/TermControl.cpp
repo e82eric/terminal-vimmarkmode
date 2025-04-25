9f9fa22e6ea7c7703701d6522ebf4da340da1d89
Eric Nelson <e82.eric@gmail.com>
2025-04-24 20:33:32 -0700

    Fix for row numbers

--- src/cascadia/TerminalControl/TermControl.cpp
+++ src/cascadia/TerminalControl/TermControl.cpp
@@ -3881,6 +3881,7 @@ namespace winrt::Microsoft::Terminal::Control::implementation
         co_await resume_foreground(Dispatcher());
         if (weakThis.get() && args)
         {
+            _updateRowNumbers();
             if (_core.HasSelection() && !args.ClearMarkers() && !_core.IsInQuickSelectMode() && !_core.IsInVimMode())
             {
                 // retrieve all of the necessary selection marker data
@@ -3951,8 +3952,6 @@ namespace winrt::Microsoft::Terminal::Control::implementation
                     displayMarker(movingEnd);
                     otherMarker.Visibility(Visibility::Collapsed);
                 }
-
-                _updateRowNumbers();
             }
             else
             {
