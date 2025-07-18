eded0119984790bcab13af05938fb2a942266a26
Eric Nelson <e82.eric@gmail.com>
2025-07-17 23:17:56 -0700

    Use simpler portable.ps1

--- portable.ps1
+++ portable.ps1
@@ -1,5 +1,9 @@
 param($deployPath = "$($home)\utilities")
 
+Import-Module .\tools\OpenConsole.psm1
+Set-MsBuildDevEnvironment
+Invoke-OpenConsoleBuild /p:Configuration=Release
+
 $ErrorActionPreference = 'stop'
 $PackageFilename = (Resolve-Path .\src\cascadia\CascadiaPackage\AppPackages\CascadiaPackage_0.0.1.0_x64_Test\CascadiaPackage_0.0.1.0_x64.msix).Path
 $WindowsTerminalPackagePath = $PackageFilename
@@ -21,29 +25,9 @@ $expandedDir = Get-ChildItem $outDir $termDirName
 $termDir = "$($deployPath)\$($termDirName)"
 $settingsDir = "$($termDir)\settings\"
 
-$settingFileToDepoy = ConvertFrom-Json (gc settings.json | Out-String)
 $existingSettingsFile = "$($settingsDir)\settings.json"
 if (Test-Path $termDir) {`
-  if (Test-Path $existingSettingsFile) {
-    Write-Host "Found existing settings file attemption to merge send input actions. Path: $($existingSettingsFile)"
-    $si = ConvertFrom-Json (gc $existingSettingsFile | Out-String)
-    $jti = $settingFileToDepoy.actions | % { $_.command.input }
-    $si.actions | % {
-      if ($jti -Contains $_.command.input) {
-        Write-Host "Found: $($_.command.input)"
-      } else {
-        Write-Host "Not Found: $($_.command.input)"
-        $settingFileToDepoy.actions += $_
-      }
-    }
-  } else {
-    Write-Host "Existing settings file not found skipping send input merge. Path: $($existingSettingsFile)"
-  }
-
   Write-Host "Found existing terminal install removing it"
   Remove-Item $termDir -Recurse -Force
 }
 Move-Item "$($expandedDir.FullName)" $termDir
-New-Item $settingsDir -Type Directory
-
-$settingFileToDepoy | ConvertTo-Json -Depth 32 | Set-Content -Path "$($settingsDir)\settings.json"
