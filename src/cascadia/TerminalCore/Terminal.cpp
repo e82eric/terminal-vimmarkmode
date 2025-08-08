cd7d9a03d5eeb44b4ab74f2463bf76a39d975270
Eric Nelson <e82.eric@gmail.com>
2025-08-07 22:21:10 -0700

    Add , to word delimiters

--- src/cascadia/TerminalCore/Terminal.cpp
+++ src/cascadia/TerminalCore/Terminal.cpp
@@ -1619,7 +1619,7 @@ std::wstring Terminal::CurrentWordPrefix() const
     const auto& buffer = _activeBuffer();
     const auto cursorPos = buffer.GetCursor().GetPosition();
 
-    const auto start = buffer.GetWordStart2({ std::max(0,cursorPos.x - 1), cursorPos.y }, L"", false);
+    const auto start = buffer.GetWordStart2({ std::max(0,cursorPos.x - 1), cursorPos.y }, L",", false);
 
     if (start == cursorPos)
     {
